374a16929864ace98a7c601da67464a2b835d2bc
LEONG WEI JUN <ambroseleong04@gmail.com>
2023-12-15 17:53:03 +0800

    Leong

--- Website/Dashboard/Lecturer/Lecturer-delete.php
+++ Website/Dashboard/Lecturer/Lecturer-delete.php
@@ -6,18 +6,38 @@ if (isset($_GET['LecturerID'])) {
     // Get the lecturer ID from the GET request
     $id = $_GET['LecturerID'];
 
-    // Delete the lecturer record from the lecturer table
-    $sql = "DELETE FROM lecturer WHERE LecturerID = $id";
-    // Check if the delete was successful
-    if ($conn->query($sql) === TRUE) {
-        // Delete the lecturer details record from the lecturer_detail table
-        $sql_lecturerdetail = "DELETE FROM lecturerdetail WHERE LecturerID = $id";
-        // Redirect the user to the lecturer page
-        header('Location: Lecturer.php');
-        exit;
-    } else {
-        // Display an error message if the delete was unsuccessful
-        echo "Error deleting record: " . $conn->error;
+
+    $sql_coursedetail = "DELETE FROM coursedetail WHERE LecturerID = ?";
+    $stmt_coursedetail = $conn->prepare($sql_coursedetail);
+    $stmt_coursedetail->bind_param("i", $id); // Assuming LecturerID is an integer
+
+    if ($stmt_coursedetail->execute()) {
+
+        // Prepare and bind the DELETE statement
+        $sql = "DELETE FROM lecturerdetail WHERE LecturerID = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $id); // Assuming LecturerID is an integer
+
+        // Execute the delete statement for lecturerdetail table
+        if ($stmt->execute()) {
+            // Prepare and bind the DELETE statement for lecturer table
+            $sql_lecturerdetail = "DELETE FROM lecturer WHERE LecturerID = ?";
+            $stmt_lecturerdetail = $conn->prepare($sql_lecturerdetail);
+            $stmt_lecturerdetail->bind_param("i", $id); // Assuming LecturerID is an integer
+
+            // Execute the delete statement for lecturer table
+            if ($stmt_lecturerdetail->execute()) {
+                // Redirect the user to the lecturer page if successful
+                header('Location: Lecturer.php');
+                exit;
+            } else {
+                // Display an error message if the delete was unsuccessful
+                echo "Error deleting record: " . $conn->error;
+            }
+        } else {
+            // Display an error message if the delete was unsuccessful
+            echo "Error deleting record: " . $conn->error;
+        }
     }
 }
 ?>
